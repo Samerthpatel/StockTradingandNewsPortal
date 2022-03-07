c7505a2a925dfc8f5e4ac7882105a6f38771970e
samerthpatel <samerth2000@gmail.com>
2022-03-07 11:59:12 -0500

    fixed styling and changed nav bar

--- website/edit_details.php
+++ website/edit_details.php
@@ -32,14 +32,76 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Public Chat - Edit Details</title>
-	<link rel="stylesheet" href="css/home.css">
-	<script src="js/home.js"></script>
+	<title>User - Profile</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" href="bootstrap-4.3.1-dist/css/style.css">
+	<link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.css">
+	<link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="bootstrap-4.3.1-dist/css/styles.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand text-success" href="#">
+              Crypto coders
+            </a>
+            <button class="navbar-toggler" type="button" 
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+  
+            <div class="collapse navbar-collapse"></div>
+  
+            <div class="collapse navbar-collapse" 
+                 id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="pages/dash.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" 
+                           href="home.php">
+                          Chat
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                           href="pages/news.php">
+                          News
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                           href="reserach.php">
+                          Research 
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                           href="trade.php">
+                          Trade 
+                        </a>
+                    </li>
+					<li class="nav-item">
+                        <a class="nav-link"
+                           href="logout.php">
+                          Logout
+                        </a>
+                    </li>
+                    <li class="nav-item">
+						<a class="nav-link" href="profile.php?userid=<?php echo $_SESSION['userid']; ?>"><?php echo $urow['your_name']; ?></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 <table id="chat_room" align="center">
 	<tr>
-		<td><a href="home.php">Home</a></td>
+		<td><a href="pages/dash.php">Home</a></td>
 	</tr>
 	<tr>
 	<th><h4>Edit Details - <a href="logout.php" onclick="return confirm_logout();">Logout</a></h4></th>
