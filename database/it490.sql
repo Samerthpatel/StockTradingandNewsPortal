179768f5ad7cea42302151f8ee5ba083dfea79f8
Samerth Patel <samerth2000@gmail.com>
2022-03-19 19:30:58 -0400

    updated .sql file

--- database/it490.sql
+++ database/it490.sql
@@ -1,8 +1,22 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 19, 2022 at 07:30 PM
+-- Server version: 8.0.28-0ubuntu0.20.04.3
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `it490`
@@ -14,14 +28,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `chat`
 --
 
-DROP TABLE IF EXISTS `chat`;
-CREATE TABLE IF NOT EXISTS `chat` (
-  `chatid` int(11) NOT NULL AUTO_INCREMENT,
-  `chat_room_id` int(11) DEFAULT NULL,
+CREATE TABLE `chat` (
+  `chatid` int NOT NULL,
+  `chat_room_id` int DEFAULT NULL,
   `chat_msg` text,
-  `userid` int(11) DEFAULT NULL,
-  `chat_date` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`chatid`)
+  `userid` int DEFAULT NULL,
+  `chat_date` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -30,12 +42,10 @@ CREATE TABLE IF NOT EXISTS `chat` (
 -- Table structure for table `chat_room`
 --
 
-DROP TABLE IF EXISTS `chat_room`;
-CREATE TABLE IF NOT EXISTS `chat_room` (
-  `chat_room_id` int(11) NOT NULL AUTO_INCREMENT,
-  `chat_room_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`chat_room_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+CREATE TABLE `chat_room` (
+  `chat_room_id` int NOT NULL,
+  `chat_room_name` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `chat_room`
@@ -47,26 +57,99 @@ INSERT INTO `chat_room` (`chat_room_id`, `chat_room_name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `stock`
+--
+
+CREATE TABLE `stock` (
+  `userid` varchar(255) NOT NULL,
+  `stockname` varchar(255) NOT NULL,
+  `stockprice` varchar(255) NOT NULL,
+  `stockshares` varchar(255) NOT NULL,
+  `total` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `tradeid` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `userid` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `userid` int NOT NULL,
   `username` varchar(30) DEFAULT NULL,
   `password` varchar(30) DEFAULT NULL,
   `your_name` varchar(60) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `balance` int NOT NULL DEFAULT '1000000'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`userid`, `username`, `password`, `your_name`, `email`, `phone`) VALUES
-(1, 'pao', 'pao', 'PaoPao', 'pao@yahoo.com', '09123123123');
+INSERT INTO `user` (`userid`, `username`, `password`, `your_name`, `email`, `phone`, `balance`) VALUES
+(2, 'samerthpatel', 'password', 'samerth', 'Srp226@njit.edu', '857684737', 1000000),
+(3, 'johndoe', 'password', 'johndoe', 'johndoe@gmail.com', '586974856', 1000000),
+(4, 'janedoe', 'password', 'janedoe', 'jane@gmail.com', '999999999', 1000000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`chatid`);
+
+--
+-- Indexes for table `chat_room`
+--
+ALTER TABLE `chat_room`
+  ADD PRIMARY KEY (`chat_room_id`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`tradeid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `chatid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `chat_room`
+--
+ALTER TABLE `chat_room`
+  MODIFY `chat_room_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `tradeid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
