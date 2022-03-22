1f40924eca36a5f3d0bcea44feb0337afea9ada7
rishirr17 <rpr32@njit.edu>
2022-03-21 23:27:52 -0400

    replacedtest

--- website/messaging/tradestock.php
+++ website/messaging/tradestock.php
@@ -103,19 +103,7 @@ session_start();
 			}
 
     }else{
-        $message="bfffffrokenffff.";
+        $message="broken.";
         echo $message;
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div>hello nigga</div>
-</body>
-</html>
