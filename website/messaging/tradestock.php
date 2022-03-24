68380b339efccbdafae9177253c96fc8031cc8fe
rishirr17 <rpr32@njit.edu>
2022-03-23 23:50:17 -0400

    added email

--- website/messaging/tradestock.php
+++ website/messaging/tradestock.php
@@ -1,5 +1,8 @@
 <?php
     session_start();
+    require 'vendor/autoload.php';
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
     if(isset($_POST['buy'])){
         require_once('../../rabbitmq/path.inc');
         require_once('../../rabbitmq/get_host_info.inc');
@@ -18,8 +21,55 @@
             $response = $client->send_request($request);
 
             if ($response == true){
+    $email = "rishiradia17@gmail.com";
+    $subject = "Trade Details";
+    $message = "Below are your trade details";
+ 
+ 
+    //Load composer's autoloader
+    require 'vendor/autoload.php';
+ 
+    $mail = new PHPMailer(true);                            
+    try {
+        //Server settings
+        $mail->isSMTP();                                     
+        $mail->Host = 'smtp.gmail.com';                      
+        $mail->SMTPAuth = true;                             
+        $mail->Username = 'cryptocoders69@gmail.com';     
+        $mail->Password = 'IT-490-class';             
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+            )
+        );                         
+        $mail->SMTPSecure = 'ssl';                           
+        $mail->Port = 465;                                   
+ 
+        //Send Email
+        $mail->setFrom('cryptocoders69@gmail.com');
+ 
+        //Recipients
+        $mail->addAddress($email);              
+        $mail->addReplyTo('cryptocoders69@gmail.com');
+ 
+        //Content
+        $mail->isHTML(true);                                  
+        $mail->Subject = $subject;
+        $mail->Body    = $message;
+ 
+        $mail->send();
+ 
+       $_SESSION['result'] = 'Message has been sent';
+	   $_SESSION['status'] = 'ok';
+    } catch (Exception $e) {
+	   $_SESSION['result'] = 'Message could not be sent. Mailer Error: '.$mail->ErrorInfo;
+	   $_SESSION['status'] = 'error';
+    }
                 echo "<script>window.alert('trade placed.')</script>";
 				header("location: ../pages/trade.php");
+
 			}
 			 else {
 				echo '<div class="alert alert-danger" style="text: center;">Error: Traded didnt go through.</div>';
@@ -44,6 +94,53 @@
             $response = $client->send_request($request);
 
             if ($response == true){
+
+    $email = "rishiradia17@gmail.com";
+    $subject = "Trade Details";
+    $message = "Below are your trade details";
+ 
+ 
+    //Load composer's autoloader
+    require 'vendor/autoload.php';
+ 
+    $mail = new PHPMailer(true);                            
+    try {
+        //Server settings
+        $mail->isSMTP();                                     
+        $mail->Host = 'smtp.gmail.com';                      
+        $mail->SMTPAuth = true;                             
+        $mail->Username = 'cryptocoders69@gmail.com';     
+        $mail->Password = 'IT-490-class';             
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+            )
+        );                         
+        $mail->SMTPSecure = 'ssl';                           
+        $mail->Port = 465;                                   
+ 
+        //Send Email
+        $mail->setFrom('cryptocoders69@gmail.com');
+ 
+        //Recipients
+        $mail->addAddress($email);              
+        $mail->addReplyTo('cryptocoders69@gmail.com');
+ 
+        //Content
+        $mail->isHTML(true);                                  
+        $mail->Subject = $subject;
+        $mail->Body    = $message;
+ 
+        $mail->send();
+ 
+       $_SESSION['result'] = 'Message has been sent';
+	   $_SESSION['status'] = 'ok';
+    } catch (Exception $e) {
+	   $_SESSION['result'] = 'Message could not be sent. Mailer Error: '.$mail->ErrorInfo;
+	   $_SESSION['status'] = 'error';
+    }
                 echo "<script>window.alert('trade placed.')</script>";
 				header("location: ../pages/trade.php");
 			}
